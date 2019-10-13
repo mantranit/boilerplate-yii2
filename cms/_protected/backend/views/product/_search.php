3df3fc42ce408728bce625292a929965d5e496eb
Jimmy Tran <jimmy.tran@watasolutions.com>
2019-10-13 18:38:26 +0700

    product/index

--- cms/_protected/backend/views/product/_search.php
+++ cms/_protected/backend/views/product/_search.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\widgets\ActiveForm;
 use common\models\Category;
 
@@ -17,18 +18,21 @@ use common\models\Category;
     ]); ?>
     <div class="row">
         <div class="col-3">
-    <?php echo $form->field($model, 'name')->textInput(['placeholder' => 'Tên sản phẩm']) ?>
+            <?php echo $form->field($model, 'name')->textInput(['placeholder' => 'Tên sản phẩm']) ?>
         </div>
         <div class="col-3">
-    <?php echo $form->field($model, 'price_init')->dropDownList(Category::getTreeView(), ['prompt' => '- Danh mục -']) ?>
+            <?php echo $form->field($model, 'price_init')->dropDownList(Category::getTreeView(), ['prompt' => '- Danh mục -']) ?>
         </div>
         <div class="col-3">
-    <?php echo $form->field($model, 'status')->dropDownList($model->getStatusList(), ['prompt' => '- Trạng thái -']) ?>
+            <?php echo $form->field($model, 'status')->dropDownList($model->getStatusList(), ['prompt' => '- Trạng thái -']) ?>
         </div>
         <div class="col-3">
-    <div class="form-group">
-        <?= Html::submitButton('<i class="fa fa-search"></i>', ['class' => 'btn btn-success']) ?>
-    </div>
+            <div class="form-group">
+                <?= Html::submitButton('<i class="fa fa-filter"></i>', ['class' => 'btn btn-success']) ?>
+                <?php if(isset(Yii::$app->request->queryParams) && count(Yii::$app->request->queryParams)) { ?>
+                <?= Html::a('<i class="fa fa-times"></i>', Url::toRoute(['index']), ['class' => 'btn btn-secondary']) ?>
+                <?php } ?>
+            </div>
         </div>
     </div>
     <?php ActiveForm::end(); ?>
